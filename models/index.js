ab89ce84904d633f4c8303ef48bfb848ea575c9a
theeshyguy <t.siopes00@yahoo.com>
2021-06-12 13:52:19 -0400

    fixed errors in index

--- models/index.js
+++ models/index.js
@@ -1,17 +1,8 @@
 const User = require('./user');
 const Subscriptions = require('./subscription');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
-
 User.hasMany(Subscriptions, {
     foreignKey: 'user_id',
 })
 
-module.exports = { User, Inputs, Subscriptions, Payments };
+module.exports = { User, Subscriptions};
