c4dd3de71ec38e09f873b13990357fbc6a09371c
Dylan Cole <logboarddjc@gmail.com>
2021-06-17 21:42:15 -0400

    add update and delete routes to subscriptions

--- controllers/api/subscriptionRoutes.js
+++ controllers/api/subscriptionRoutes.js
@@ -15,4 +15,39 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updateSub = await Subscription.update(req.body, {
+      where: {
+        user_id: req.session.user_id,
+        id: req.body.id,
+      },
+    });
+
+    res.status(200).json(updateSub);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) =>{
+  try {
+    const subData = await Subscription.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!subData) {
+      res.status(404).json({ message: 'No subscription found with this id!' });
+      return;
+    }
+
+    res.status(200).json(subData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
