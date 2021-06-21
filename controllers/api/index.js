1cc99e07e63732cc645546e5d7a99a05f1aaf33c
Brendan Tyler <bkrendan12@gmail.com>
2021-06-21 19:51:43 -0400

    routing renaming

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const subscriptionRoute = require('./subscriptionRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userroutes");
+const subscriptionRoute = require("./subscriptionroutes");
 
-router.use('/users', userRoutes);
-router.use('/subscriptions', subscriptionRoute);
+router.use("/users", userRoutes);
+router.use("/subscriptions", subscriptionRoute);
 
 module.exports = router;
