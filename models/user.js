6f04f6f5e8a3233a4f25ca2e73bc37175ce5cbe3
theeshyguy <t.siopes00@yahoo.com>
2021-06-21 22:07:50 -0400

    reworked connection system

--- models/user.js
+++ models/user.js
@@ -1,6 +1,24 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection");
+// const sequelize = require("../config/connection");
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 class User extends Model {
   checkPassword(loginPw) {
