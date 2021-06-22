66797b8d7f08ab578ad974bc585539523b7c720c
theeshyguy <t.siopes00@yahoo.com>
2021-06-21 20:48:39 -0400

    messing with configuration

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
+const config = require("config");
 
 let sequelize;
 
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+if (config.production.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.production.use_env_variable], config.production);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
